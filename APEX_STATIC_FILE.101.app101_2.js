eab412dee04e99508c5979a1cdaae9d621c58aec
mark brown <mark.russellbrown@gmail.com>
2024-04-13 21:01:19 +0100

    Commit by DB202103270929

--- APEX_STATIC_FILE.101.app101_2.js
+++ APEX_STATIC_FILE.101.app101_2.js
@@ -315,11 +315,11 @@ window.addEventListener("DOMContentLoaded", () => {
     wrapper.addEventListener("focusin",focusHandler);
     wrapper.addEventListener("change",changeHandler);
     
-
+    /*
     if (checkPerformance()) {
         const observer = new PerformanceObserver(perfObserver);
         observer.observe({ type: "resource", buffered: true });
-    }
+    }*/
 });
 
 const lazyload = () => {
@@ -348,56 +348,6 @@ const lazyload = () => {
         }
         img.src = src;
     }
-}
-
-/*
-**  BATCH PERFORMANCE ENTRIES IN ARRAY TO AVOID TOO MANY DATABASE PROCESS CALLS
-*/
-const perfObserver = (list) => {
-    list.getEntries().forEach((entry) => {
-        /* 
-        ** Process network media transfers - ignore cache
-        ** Safari does not publish transferSize so we handle that in the HEAD fetch
-        ** Transfers <=300 are treated as cache transfers
-        ** HEAD fetch retrieves content-type for all browsers as this is reported inaccurately for Cloudinary asset downloads
-        */ 
- 
-        if (["img","video","audio"].indexOf(entry.initiatorType)===-1) return;
-
-        if (entry.transferSize > 0 || gBrowser.startsWith("Safari")) {
-            const duration = Math.round(entry.duration * 1) / 1;
-            
-            const parts = entry.name.split("/");
-            let public_id = parts.pop();
-            const resource_type = parts[4] === "video" ? 'video' : "image";
-
-            const filetype = public_id.substring(public_id.lastIndexOf(".")+1);
-            if (['png','jpg','webm','mp4','aac','ogg','mp3','wav'].indexOf(filetype)>=0) {
-                public_id = public_id.substring(0,public_id.lastIndexOf("."));
-            }
-
-            // Get the media content-type with HEAD fetch. Wish I knew how Developer Tools do it.
-            fetch(entry.name,{method:'HEAD', headers: {Accept: "image/avif,image/webp,image/apng,image/svg+xml,image/*,video/webm,video/mp4,audio/aac,audio/ogg,audio/mp3,audio/wav",}})
-                .then(response => {
-                    const contentType = response.headers.get('Content-Type');
-                    let transferSize = entry.transferSize;
-                    if (gBrowser.startsWith("Safari")) {
-                        transferSize = response.headers.get('Content-Length');
-                    }
-                    //gPerfObj.images.push(
-                    mediaQueue.add(
-                        {"session_id": apex_session, "cld_cloud_name": parts[3], "resource_type": resource_type, "public_id": public_id, "epoch": Math.round(Date.now()/1000),
-                        "url": entry.name, "transfersize": transferSize, "duration": duration, "content_type":contentType,
-                        "window_innerwidth": window.innerWidth, "browser":gBrowser, "servertiming": entry.serverTiming}
-                    );
-                })
-                .catch(error => {
-                    console.error('Error fetching media content-type:', error);
-                    popupOpen('Error fetching media content-type:', error);
-                });
-        };
-        
-    });
 }
 
 /* 
@@ -1065,9 +1015,11 @@ ClassicEditor.create(document.querySelector("#editor"), {
     })
     .then( (newEditor) => {
         editor = newEditor;
+        
         const wordCountPlugin = editor.plugins.get( 'WordCount' );
-        const wordCountWrapper = document.getElementById( 'word-count' );
-        wordCountWrapper.appendChild( wordCountPlugin.wordCountContainer );
+        const wordCountWrapper = wrapper.querySelector( '.ck-editor__main' );
+        wordCountWrapper.appendChild(wordCountPlugin.wordCountContainer );
+
         editor.editing.view.document.on( 'drop', ( evt, data ) => {
 			// Stop execute next callbacks.
 			evt.stop();
@@ -1086,6 +1038,23 @@ ClassicEditor.create(document.querySelector("#editor"), {
         editor_status = "init";
         editor_status_text = wrapper.querySelector("#editor-status");
         editor.enableReadOnlyMode( 'lock-id' );
+
+        const editor_content = wrapper.querySelector(".ck-editor__editable_inline").getBoundingClientRect();
+        console.log("editor_content",editor_content)
+
+        const word_count = wrapper.querySelector(".ck-word-count").getBoundingClientRect();
+        console.log("word_count",word_count)
+
+        const wrapper_rect = wrapper.getBoundingClientRect();
+        console.log("wrapper",wrapper_rect);
+
+        const editor_height = window.innerHeight - editor_content.y - word_count.height;
+        console.log("editor_height",editor_height)
+        
+        /*wrapper.querySelector(".ck-editor__editable_inline").style.height = editor_height+"px";*/
+        editor.editing.view.change( writer => {
+            writer.setStyle( 'height',editor_height+"px" , editor.editing.view.document.getRoot() );
+        } );
     })
     .catch(error => {
         console.error(error);
