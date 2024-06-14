6dfd9d18775f4c65e383dfc1664ab2db115da99c
mark brown <mark.russellbrown@gmail.com>
2024-06-14 22:01:22 +0100

    Commit by DB202103270929

--- APEX_STATIC_FILE.101.deploy_cart.js
+++ APEX_STATIC_FILE.101.deploy_cart.js
@@ -4,6 +4,18 @@
 const cart_total_ele = document.querySelector("#cart-total");
 let cart_total = 0;
 let purchases =[];
+
+const setQuantity = (button, quantity) => {
+  
+  const span = button.querySelector("span");
+  if (!span) console.log(button);
+  if (quantity===0) {
+    span.textContent = "";
+  } else {
+    span.textContent = "(" + quantity + "x)";
+  }
+}
+
 document.querySelectorAll("button.buy").forEach((button) => {
     button.addEventListener("click", (e) => {
       cart_total+=Number(button.dataset.price);
@@ -13,11 +25,16 @@ document.querySelectorAll("button.buy").forEach((button) => {
         const url = e.target.parentElement.querySelector("img").src;
         purchases.push({url:url,name:button.dataset.name, price:button.dataset.price, currency:button.dataset.currency, quantity:0});
       }
+      let quantity;
       purchases.forEach(purchase => {
         if (purchase.name === button.dataset.name) {
             purchase.quantity = Number(purchase.quantity) + 1;
+            quantity = purchase.quantity;
         }
       });
+      
+      setQuantity(e.target, quantity);
+      
     });
 });
 
@@ -26,7 +43,7 @@ const cart_items = document.getElementById("cart-items"); /* dialog to contain c
 document.getElementById("cart-show").addEventListener("click",() => {
   if (cart_total===0) return;
   
-  let items = "<table>";
+  let items = `<table><caption class="close">x</caption>`;
   purchases.forEach(purchase => {
     items += `<tr><td rowspan="2"><img src="${purchase.url}"></td><td>${purchase.name}<button type="button" class="delete" data-name="${purchase.name}"><svg><use href="#delete"></use></svg></button></td></tr>`;
     items += `<tr><td><span>${purchase.price} ${purchase.currency}</span><input type="number" name="quantity" min="1" max="20" step="1" value="${purchase.quantity}" data-name="${purchase.name}" data-price="${purchase.price}"></td></tr>`;
@@ -48,34 +65,48 @@ const changeHandler = (e) => {
     cart_total+=Number(purchase.price)*Number(purchase.quantity);
   });
   cart_total_ele.textContent = cart_total.toFixed(2) + " CHF";
+  
+  const button = document.querySelector("button.buy[data-name='" + name + "']");
+  
+  setQuantity(button, e.target.value);
+  
 }
 
-const deleteHandler = (e) => {
-  if (!e.target.matches(".delete")) return;
+const clickHandler = (e) => {
+  if (e.target.matches(".close")) {
+    cart_items.close();
+    return;
+  }
   
-    /* remove item from UI */
-    const tr1 = e.target.closest("tr"),
-          tr2 = tr1.nextElementSibling;
-    tr1.remove();
-    tr2.remove();
-  
+  if (e.target.matches(".delete")) {
      /* remove item from array*/
-    
     purchases.forEach((purchase,i) => {
       if (purchase.name === e.target.dataset.name) {
         purchases.splice(i, 1);
       }
     });
+
+    /* recalculate total */
     cart_total = 0;
     purchases.forEach((purchase,i) => {
       cart_total+=Number(purchase.price)*Number(purchase.quantity);
     });
-  
+
+    /* Update UI */
+    const button = document.querySelector("button.buy[data-name='" + e.target.dataset.name + "']");
+    setQuantity(button,0);
     cart_total_ele.textContent = cart_total.toFixed(2) + " CHF";
     if (cart_total===0) {
+      cart_items.querySelector("table").remove();
       cart_items.close();
+    } else {
+      const tr1 = e.target.closest("tr"),
+            tr2 = tr1.nextElementSibling;
+      tr1.remove();
+      tr2.remove();
     }
+  }
 }
 
 cart_items.addEventListener("change",changeHandler);
-cart_items.addEventListener("click",deleteHandler);
+cart_items.addEventListener("click",clickHandler);
