bbd6053c38248b0564beba4240a20313d5e3853d
mark brown <mark.russellbrown@gmail.com>
2024-09-01 22:01:23 +0100

    Commit by DB202103270929

--- APEX_STATIC_FILE.101.deploy_contact.js
+++ APEX_STATIC_FILE.101.deploy_contact.js
@@ -1,3 +1,6 @@
+/*
+** CONTACT FORM SUBMISSION
+*/
 const sendContact = async () => {
   const formData = new FormData(form);
   try {
@@ -5,22 +8,17 @@ const sendContact = async () => {
       method: "POST",
       body: JSON.stringify(Object.fromEntries(formData))
     });
-    const result = await response.json();
-
-    const formBtn = form.querySelector("button");
-    formBtn.disabled = true;
-    formBtn.style.cursor = "none";
-    formBtn.querySelector(".edge").remove();
-    formBtn.querySelector(".shadow").remove();
     
-    const front = formBtn.querySelector(".front");
-    form.querySelector(".result").style.opacity = "1";
-      
+    const result = await response.json();
+    const result_msg = form.querySelector(".result");
+    
     if (result.success) {
-        front.textContent = "Sent";
-        front.style.background = "green";
-    } else {
-        front.textContent = "Failed - copy emaail address and send manually";
+      const formBtn = form.querySelector("button");
+      formBtn.disabled = true;
+      formBtn.style.cursor = "none";
+      formBtn.textContent = "SENT";
+      formBtn.style.backgroundColor = "green";
+      result_msg.textContent = "Thank You!";
     }
     
   } catch (e) {
@@ -28,30 +26,12 @@ const sendContact = async () => {
   }
 }
 
-/*
-** CONTACT FORM SUBMISSION
-*/
-const form = document.querySelector("div.contact > form"),
-      copy_content = form.querySelector(".copy-content");
+
+const form = document.querySelector("form.contact");
 
 form.querySelector("[name='url']").value = window.location.hostname; /* includes website in submission */
 
 form.addEventListener("submit", (e) => {
     e.preventDefault();
     sendContact();
-});
-                      
-/*
-** COPY TEXT CONTENT OF BUTTON TO CLIPBOARD WHEN CLICKED 
-*/
-copy_content.addEventListener("click", async () => {
-    const text = copy_content.innerText;
-    const result = copy_content.querySelector("span");
-    try {
-        await navigator.clipboard.writeText(text);
-        result.textContent = "Copied";
-        result.style.opacity = "1";
-    } catch (error) {
-        result.textContent = error.message;
-    }
 });
