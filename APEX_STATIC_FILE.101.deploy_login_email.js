480e76add6dd367d4ce499bbf4deb5f90ebf683b
mark brown <mark.russellbrown@gmail.com>
2024-11-07 21:01:17 +0000

    Updated using DBMS_CLOUD_REPO by user "EXAMPLE" from database HL7OFFZWEZQ2CAL_DB202103270929

--- APEX_STATIC_FILE.101.deploy_login_email.js
+++ APEX_STATIC_FILE.101.deploy_login_email.js
@@ -2,16 +2,25 @@
 ** LOGIN WITH EMAIL FORM SUBMISSION
 */
 
-const callAPI = async (url, method) => {
-    const formData = new FormData(form);
+const callAPI = async (endpoint, method, token) => {
+    
+    const url = bodydata.resturl + endpoint + "/" +bodydata.websiteid;
 
-    const response = await fetch(url, {
-        method: method,
-        body: JSON.stringify(Object.fromEntries(formData))
-    });
+    let headers = new Headers();
+    if (token) {
+        headers.append("Authorization","Bearer " + token);
+    }
+
+    let config = {method: method, headers: headers};
+    if (method==="POST" || method==="PUT") {
+        const formData = new FormData(form);
+        config["body"] = JSON.stringify(Object.fromEntries(formData));
+    }
+
+    const response = await fetch(url, config);
 
     if (!response.ok) {
-        throw Error(`System Error: ${response.status} - ${response.message}`);
+        throw Error(`System Error: ${response.status} - ${response.message}}`);
     }
 
     const data = await response.json();
@@ -25,9 +34,60 @@ const callAPI = async (url, method) => {
     return(data);
 }
 
+
 const bodydata = document.body.dataset;
-const url = bodydata.resturl + "authenticate/" +bodydata.websiteid;
 const form = document.querySelector("form.login");
+
+/* sendcode elements hidden until code has been emailed */
+form.querySelectorAll(".sendcode").forEach((ele) => {
+    ele.style.display = "none";
+});
+
+/* CHECK IF USER ALREADY LOGGED IN. REFRESH TOKEN IF EXPIRED */
+let token = sessionStorage.getItem("token");
+if (token) {
+    console.log("Using session token");
+} else {
+    token = localStorage.getItem("refresh");
+    if (token) {
+        console.log("Using refresh token");
+    }
+}
+
+if (token) {
+    form.style.display = "none";
+    const article = document.querySelector("article:first-of-type");
+    article.replaceChildren();
+    article.insertAdjacentHTML('beforeend',"<p>Already logged in</p>");
+
+    callAPI("authenticate", "GET", token)
+        .then((data) => {
+            if (data.message==="expired") {
+                article.insertAdjacentHTML('beforeend',"<p>token expired - get new access token using refresh token</p>");
+                return callAPI("authenticate-refresh", "POST", localStorage.getItem("refresh"));
+            }
+            article.insertAdjacentHTML('beforeend',"<p>" + data.email + " with " + data.scope + " privilege</p>");
+            return Promise.resolve();
+        })
+        .then((data) => {
+            if (!data) return;
+            if (data.message==="login") {
+                article.insertAdjacentHTML('beforeend',"<p>Refresh token has expired. Have to login again.</p>");
+                sessionStorage.removeItem("token");
+                localStorage.removeItem("refresh");
+            } else {
+                article.insertAdjacentHTML('beforeend',"<p>" + data.email + ":" + data.scope + "</p>");
+                sessionStorage.setItem("token",data.token);
+                localStorage.setItem("refresh",data.refresh);
+                form.style.display = "block";
+            }
+        })
+        .catch((error) => {
+            article.insertAdjacentHTML('beforeend',"<p>" + error + "</p>");
+        })
+} else {
+
+
 const sendmail_btn = form.querySelector("button.sendmail"),
       sendmail_msg = form.querySelector(".sendmail-result"),
       sendcode_btn = form.querySelector("button.sendcode"),
@@ -36,37 +96,38 @@ const sendmail_btn = form.querySelector("button.sendmail"),
 /* Include website url in submitted form data */
 form.querySelector("[name='url']").value = window.location.hostname; 
 
-/* sendcode elements hidden until code has been emailed */
-form.querySelectorAll(".sendcode").forEach((ele) => {
-    ele.style.display = "none";
-});
-
 form.addEventListener("submit", (e) => {
     e.preventDefault();
 });
 
 /* Submit email address - hide send button and display elements to process passcode */
 sendmail_btn.addEventListener("click", () => {
-    callAPI(url,"POST").then((data) => {
-        sendmail_msg.textContent = data.message;
-        sendmail_btn.style.display = "none";
-        form.querySelectorAll(".sendcode").forEach((ele) => {
-            ele.style.display = "block";
+    callAPI("authenticate", "POST")
+        .then((data) => {
+            sendmail_msg.textContent = data.message;
+            sendmail_btn.style.display = "none";
+            form.querySelectorAll(".sendcode").forEach((ele) => {
+                ele.style.display = "block";
+            });
+        })
+        .catch((error) => {
+            sendmail_msg.textContent = error;
+            sendmail_msg.style.color = "red";
         });
-    }).catch((error) => {
-        sendmail_msg.textContent = error;
-        sendmail_msg.style.color = "red";
     });
-});
 
 sendcode_btn.addEventListener("click", () => {
-    callAPI(url,"PUT").then((data) => {
-        sessionStorage.setItem("token",data.token);
-        sendcode_msg.textContent = data.message;
-        sendcode_msg.style.color = "green";
-        sendcode_btn.style.display = "none";
-    }).catch((error) => {
-        sendcode_msg.textContent = error;
-        sendcode_msg.style.color = "red";
+    callAPI("authenticate", "PUT")
+        .then((data) => {
+            sessionStorage.setItem("token",data.token);
+            localStorage.setItem("refresh",data.refresh);
+            sendcode_msg.textContent = data.message;
+            sendcode_msg.style.color = "green";
+            sendcode_btn.style.display = "none";
+        })
+        .catch((error) => {
+            sendcode_msg.textContent = error;
+            sendcode_msg.style.color = "red";
+        });
     });
-});
+}
