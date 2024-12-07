75dc6961c03947516af68619b55bb75246eb693b
mark brown <mark.russellbrown@gmail.com>
2024-12-07 21:01:14 +0000

    Updated using DBMS_CLOUD_REPO by user "EXAMPLE" from database HL7OFFZWEZQ2CAL_DB202103270929

--- APEX_STATIC_FILE.101.deploy_blog.js
+++ APEX_STATIC_FILE.101.deploy_blog.js
@@ -1,7 +1,7 @@
 /*
 ** MAKE ENTIRE CARD ELEMENTS CLICKABLE
 */
-document.querySelectorAll('.card').forEach((card) => {
+document.querySelectorAll('.card:not(:has(h1))').forEach((card) => {
     card.style.cursor = 'pointer';
     card.addEventListener("click", () => {
         card.querySelector('h2 a').click();
