f7a6b939ab78a9026b1e69c4e1b60bddeeda6fc3
mark brown <mark.russellbrown@gmail.com>
2025-01-01 21:01:23 +0000

    Updated using DBMS_CLOUD_REPO by user "EXAMPLE" from database HL7OFFZWEZQ2CAL_DB202103270929

--- APEX_STATIC_FILE.101.deploy_leave_message.js
+++ APEX_STATIC_FILE.101.deploy_leave_message.js
@@ -1,47 +1,44 @@
-
 /*
-**  LEAVE A MESSAGE DIALOG
+**  LEAVE A MESSAGE
 */
-export const process_message = (endpoint,method) => {
-  
-    const messageInput = document.getElementById("messageInput");
-    const messageError = document.querySelector(".messageInput-result");
-    const message = document.querySelector("dialog.message");
-    const sendmessage = message.querySelector(".send");
-    const charcounter = message.querySelector(".charcounter");
-    const maxchars = messageInput.getAttribute("maxlength");
+import { message_dialog } from "./deploy_elements.min.js";
+import { callAPI } from "./deploy_callAPI.min.js";
 
-    message.showModal();
-  
-    messageInput.addEventListener("input", (e) => {
-        let numOfEnteredChars = messageInput.value.length;
-        charcounter.textContent = numOfEnteredChars + "/" + maxchars;
+const form = message_dialog.querySelector("form");
+const messageInput = form.querySelector("[name='message']");
+const messageError = form.querySelector(".messageInput-result");
+const sendmessage = form.querySelector(".send");
+const charcounter = form.querySelector(".charcounter");
+const maxchars = messageInput.getAttribute("maxlength");
 
-        if (numOfEnteredChars === Number(maxchars)) {
-            charcounter.style.color = "red";
-        } else {
-            charcounter.style.color = "initial";
-        }
-    });
-  
-    sendmessage.addEventListener("click", (event) => {
-        if (messageInput.validity.valueMissing) {
-            messageError.textContent = "You need to enter a message.";
+messageInput.addEventListener("input", (e) => {
+    let numOfEnteredChars = messageInput.value.length;
+    charcounter.textContent = numOfEnteredChars + "/" + maxchars;
+
+    if (numOfEnteredChars === Number(maxchars)) {
+        charcounter.style.color = "red";
+    } else {
+        charcounter.style.color = "initial";
+    }
+});
+
+sendmessage.addEventListener("click", (event) => {
+    if (messageInput.validity.valueMissing) {
+        messageError.textContent = "You need to enter a message.";
+        messageError.style.color = "red";
+        return;
+    }
+    const formData = new FormData(form);
+    const formObj = Object.fromEntries(formData);
+    callAPI(endpoint, method, formObj)
+        .then(() => {
+            const sendresult = message.querySelector(".send-result");
+            sendresult.textContent = "Successfully Sent";
+            sendresult.style.color = "green";
+            sendmessage.disabled = true;
+        })
+        .catch((error) => {
+            messageError.textContent = error;
             messageError.style.color = "red";
-            return;
-        }
-        const formData = new FormData(message.querySelector("form"));
-        const formObj = Object.fromEntries(formData);
-        callAPI(endpoint, method, formObj)
-            .then(() => {
-                const sendresult = message.querySelector(".send-result");
-                sendresult.textContent = "Successfully Sent";
-                sendresult.style.color = "green";
-                sendmessage.disabled = true;
-            })
-            .catch((error) => {
-                messageError.textContent = error;
-                messageError.style.color = "red";
-            });
-    });
-}
+        });
+});
