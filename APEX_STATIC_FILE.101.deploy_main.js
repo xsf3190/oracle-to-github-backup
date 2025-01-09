793abdd67d1ae0200e5f6da587dc2f1541c154bf
mark brown <mark.russellbrown@gmail.com>
2025-01-09 21:01:27 +0000

    Updated using DBMS_CLOUD_REPO by user "EXAMPLE" from database HL7OFFZWEZQ2CAL_DB202103270929

--- APEX_STATIC_FILE.101.deploy_main.js
+++ APEX_STATIC_FILE.101.deploy_main.js
@@ -1,24 +1,17 @@
 /*
-** MAIN JAVASCRIPT MODULE SENT ON CONNECTION TO WEBSITE
+** MAIN JAVASCRIPT MODULE ALWAYS LOADED ON CONNECTION TO WEBSITE
 */
 
-import { login_btn, email, expires, dropdown, menulist } from "./deploy_elements.min.js";
+import { dropdown } from "./deploy_elements.min.js";
 
 /*
 ** SET DROPDOWN ELEMENTS IF REFRESH TOKEN EXISTS
 */
 if (localStorage.getItem("refresh")) {
-    const arrayToken = localStorage.getItem("refresh").split(".");
-    const parsedToken = JSON.parse(atob(arrayToken[1]));
-    email.textContent = parsedToken.sub;
-    expires.textContent = new Date(parsedToken.exp*1000).toLocaleString();
-    login_btn.textContent="Log Out";
-    menulist.replaceChildren();
-    if (localStorage.getItem("menulist")) {
-        menulist.insertAdjacentHTML('afterbegin',localStorage.getItem("menulist"));
-    } else {
-        login_btn.click(); // Force logout
-    }
+    import("/deploy_menulist.min.js")
+        .then((module) => {
+            module.init("menulist/:ID");
+        });
 }
             
 /*
