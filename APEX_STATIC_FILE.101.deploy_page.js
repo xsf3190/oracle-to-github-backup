2f4d7131685c852bd460dc276a1c7f7df75cd7fe
mark brown <mark.russellbrown@gmail.com>
2025-04-04 23:00:37 +0200

    Updated using DBMS_CLOUD_REPO by user "EXAMPLE" from database HL7OFFZWEZQ2CAL_DB202103270929

---
+++ APEX_STATIC_FILE.101.deploy_page.js
@@ -0,0 +1,54 @@
+/*
+**  ADD / CHANGE / DELETE PAGE
+*/
+import { callAPI } from "./deploy_callAPI.min.js";
+
+const page_dialog = document.querySelector("dialog.page");
+const form = page_dialog.querySelector("form");
+const pageInput = form.querySelector("[name='navigation_label']");
+const pageResult = form.querySelector(".pageInput-result");
+const footer = form.querySelector("footer");
+const charcounter = form.querySelector(".charcounter");
+const maxchars = pageInput.getAttribute("maxlength");
+const loader = form.querySelector(".loader");
+
+let endpoint;
+let currentPage;
+
+export const init = (element) => {
+    endpoint = element.dataset.endpoint;
+    form.reset();
+    currentPage = document.querySelector("[aria-current='page']");
+    pageInput.value = currentPage.textContent;
+    pageResult.textContent = "";
+    page_dialog.showModal();
+}
+
+pageInput.addEventListener("input", () => {
+    let numOfEnteredChars = pageInput.value.length;
+    charcounter.textContent = numOfEnteredChars + "/" + maxchars;
+    if (numOfEnteredChars === Number(maxchars)) {
+        charcounter.style.color = "red";
+    } else {
+        charcounter.style.color = "initial";
+    }
+});
+
+footer.addEventListener("click", (e) => {
+    if (pageInput.validity.valueMissing) {
+        pageResult.textContent = "You need to enter a page label";
+        pageResult.style.color = "red";
+        return;
+    }
+    loader.classList.remove("visually-hidden");
+    const formData = new FormData(form);
+    const formObj = Object.fromEntries(formData);
+    callAPI(endpoint, e.target.dataset.method, formObj)
+        .then((data) => {
+            window.location.replace(data.href);
+        })
+        .catch((error) => {
+            pageResult.textContent = error;
+            pageResult.style.color = "red";
+        });
+});
