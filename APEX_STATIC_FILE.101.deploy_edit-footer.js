816d1362cddca413e8d17938e6be8cc23c94f6e9
mark brown <mark.russellbrown@gmail.com>
2025-04-07 22:00:27 +0100

    Updated using DBMS_CLOUD_REPO by user "EXAMPLE" from database HL7OFFZWEZQ2CAL_DB202103270929

--- APEX_STATIC_FILE.101.deploy_edit-footer.js
+++ APEX_STATIC_FILE.101.deploy_edit-footer.js
@@ -11,24 +11,69 @@ let endpoint;
 export const init = (element) => {
     endpoint = element.dataset.endpoint;
 
-    if (!document.querySelector("head > link[href='/website_edit.css']")) {
-        const link_edit = document.createElement('link');
-        link_edit.setAttribute('rel', 'stylesheet');
-        link_edit.setAttribute('href', '/website_edit.css');
-        document.head.appendChild(link_edit);
-    }
-
     callAPI(endpoint,'GET')
         .then((data) => {
             editor.insertAdjacentHTML('afterbegin',data.html);
             dropdown_details.removeAttribute("open");
             editor.scrollIntoView();
+            footer.replaceChildren();
+            footer.insertAdjacentHTML('afterbegin',data.footer);
         })
         .catch((error) => {
             handle_error(error);
         });
 }
 
+/*
+**  REFLECT USER INPUT DIRECTLY IN FOOTER ELEMENTS
+*/
+editor.addEventListener("input", (e) => {
+    const name = e.target.getAttribute("name");
+    const target = footer.querySelector("[data-" + name + "]");
+    /*
+    ** target is <a href=""><svg></svg><span></span></a>
+    */
+    switch (name) {
+        case "linkedin_name":
+            target.childNodes[1].textContent = "Let's Connect";
+            break;
+        case "instagram_name":
+            target.childNodes[1].textContent = "Connect Instagram";
+            break;
+        default:
+            target.childNodes[1].textContent = e.target.value;
+    }
+});
+
+/*
+**  UPDATE HREF IN CASE USER TESTS
+*/
+editor.addEventListener("change", (e) => {
+    const name = e.target.getAttribute("name");
+    const target = footer.querySelector("[data-" + name + "]");
+
+    target.target = "_blank";
+
+    switch (name) {
+        case "contact_email":
+            target.href = "mailto:" + e.target.value;
+            break;
+        case "telephone_number":
+            target.href = "tel:" + e.target.value;
+            break;
+        case "mobile_number":
+            target.href = "tel:" + e.target.value;
+            break;
+        case "linkedin_name":
+            target.href = "https://linkedin.com/in/" + e.target.value;
+            break;
+        case "instagram_name":
+            target.href = "https://instagram.com//" + e.target.value;
+            break;
+        default:
+            console.error("footer element not supported");
+    }
+});
 
 editor.addEventListener("click", async (e) => {
     if (e.target.matches(".cancel-changes")) {
@@ -36,10 +81,23 @@ editor.addEventListener("click", async (e) => {
         return;
     }
 
+    if (e.target.matches(".save-changes")) {
+        const formData = new FormData(editor);
+        const formObj = Object.fromEntries(formData);
+        await callAPI(endpoint,'PUT', formObj)
+            .then(() => {
+                const result = editor.querySelector(".result");
+                result.textContent = 'Changes Saved';
+                result.style.color = "green";
+            })
+            .catch((error) => {
+                handle_error(error);
+            });
+    }
+
     if (e.target.matches(".publish-changes")) {
         const formData = new FormData(editor);
         const formObj = Object.fromEntries(formData);
-        console.log("formObj",formObj);
         await callAPI(endpoint,'PUT', formObj)
             .then(() => {
                 console.log("Form changes saved");
@@ -48,8 +106,7 @@ editor.addEventListener("click", async (e) => {
                 handle_error(error);
             });
 
-        const module_name = e.target.dataset.endpoint;
-        const import_module_name = "./deploy_" + module_name.substring(0,module_name.indexOf("/")) + ".min.js";
+        const import_module_name = "./deploy_publish-website.min.js";
         await import(import_module_name)
             .then((module) => {
                 module.init(e.target);
