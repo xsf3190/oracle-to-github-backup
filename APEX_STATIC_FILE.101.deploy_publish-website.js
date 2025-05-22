7d26c2e929b5d07d179327725d8ef743a8e1050c
mark brown <mark.russellbrown@gmail.com>
2025-05-22 23:00:45 +0200

    Updated using DBMS_CLOUD_REPO by user "EXAMPLE" from database HL7OFFZWEZQ2CAL_DB202103270929

--- APEX_STATIC_FILE.101.deploy_publish-website.js
+++ APEX_STATIC_FILE.101.deploy_publish-website.js
@@ -1,8 +1,61 @@
 /*
-**  PUBLISH WEBSITE
+** USER CLICKS DEPLOY BUTTON
 */
-import { deploy_website } from "deploy_edit-content";
 
-export const init = () => {
-    deploy_website();
+import { callAPI, handleError } from "deploy_callAPI";
+import { dropdown_details } from "deploy_elements";
+
+const info_dialog = document.querySelector("dialog.info");
+
+let endpoint;
+let intervalId;
+
+export const init = (element) => {
+    if (dropdown_details.open) {
+        dropdown_details.removeAttribute("open");
+    }
+    
+    endpoint = element.dataset.endpoint;
+    
+    
+    const content = info_dialog.querySelector("article");
+    
+    callAPI(endpoint,"POST",{})
+        .then( (data) => {
+            content.replaceChildren();
+            content.insertAdjacentHTML('afterbegin',data.content);
+            info_dialog.querySelector("h4").textContent = "";
+            info_dialog.querySelector("footer").replaceChildren();
+            info_dialog.showModal();
+            if (data.stop) return;
+            if (intervalId) {
+                clearInterval(intervalId);
+            }
+            info_dialog.addEventListener("close", () => {
+                window.location.reload();
+            })
+            intervalId = setInterval(getDeploymentStatus,2000);
+        })
+        .catch((error) => {
+            handleError(error);
+        });;
+}
+
+/*
+** SHOW NETLIFY DEPLOYMENT PROGRESS
+*/
+const getDeploymentStatus = () => {
+    callAPI(endpoint,"GET")
+        .then( (data) => {
+            if (data.content) {
+                info_dialog.querySelector(".deploy").insertAdjacentHTML('beforeend',data.content);
+                info_dialog.querySelector(".deploy>li:last-child").scrollIntoView();
+            }
+            if (data.completed) {
+                clearInterval(intervalId);
+            }
+        })
+        .catch((error) => {
+            handleError(error);
+        })
 }
