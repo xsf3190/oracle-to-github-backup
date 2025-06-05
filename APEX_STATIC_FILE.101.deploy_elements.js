e565530975dce6eb3f48e142f1d02fd00b6e2074
mark brown <mark.russellbrown@gmail.com>
2025-06-05 22:00:30 +0100

    Updated using DBMS_CLOUD_REPO by user "EXAMPLE" from database HL7OFFZWEZQ2CAL_DB202103270929

--- APEX_STATIC_FILE.101.deploy_elements.js
+++ APEX_STATIC_FILE.101.deploy_elements.js
@@ -5,7 +5,6 @@ export const dropdown = document.querySelector(".dropdown-content");
 export const email = dropdown.querySelector(".email");
 export const expires = dropdown.querySelector(".expires");
 export const login_btn = dropdown.querySelector(".login-btn");
-export const menulist = dropdown.querySelector(".menulist");
 export const login_dialog = document.querySelector("dialog.login-email");
 
 export const nav = document.querySelector(".topnav");
