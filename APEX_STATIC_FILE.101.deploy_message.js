840bdef20d4214711c2b8c70675db8a56d319f39
mark brown <mark.russellbrown@gmail.com>
2025-07-31 22:00:27 +0100

    Updated using DBMS_CLOUD_REPO by user "EXAMPLE" from database HL7OFFZWEZQ2CAL_DB202103270929

--- APEX_STATIC_FILE.101.deploy_message.js
+++ APEX_STATIC_FILE.101.deploy_message.js
@@ -1,26 +1,48 @@
 /*
-**  LEAVE A MESSAGE
+**  LEAVE A MESSAGE (MUST BE AUTHENTICATED)
 */
-import { callAPI } from "deploy_callAPI";
+import { dropdown_details, output_dialog } from "deploy_elements";
+import { callAPI, handleError } from "deploy_callAPI";
 
-const message_dialog = document.querySelector("dialog.message");
-const form = message_dialog.querySelector("form");
-const messageInput = form.querySelector("[name='message']");
-const messageError = form.querySelector(".messageInput-result");
-const sendmessage = form.querySelector(".send");
-const sendresult = form.querySelector(".send-result");
-const charcounter = form.querySelector(".charcounter");
-const maxchars = messageInput.getAttribute("maxlength");
+const header = output_dialog.querySelector("header");
+const article = output_dialog.querySelector("article");
+const footer = output_dialog.querySelector("footer");
 
-let endpoint, method;
+let messageInput, messageError, charcounter, maxchars;
+
+let endpoint;
 
 export const init = (element) => {
     endpoint = element.dataset.endpoint;
-    method = element.dataset.method;
-    message_dialog.showModal();
+    dropdown_details.removeAttribute("open");
+
+    callAPI(endpoint, "GET")
+    .then((data) => {
+        header.querySelector(":first-child").replaceChildren();
+        header.insertAdjacentHTML('afterbegin',data.header);
+        article.replaceChildren();
+        article.insertAdjacentHTML('afterbegin',data.article);
+        footer.replaceChildren();
+        footer.insertAdjacentHTML('afterbegin',data.footer);
+
+        /* SET VARIABLES FOR INTERACTIVE ELEMENTS REFERENCED IN EVENT HANDLERS */
+        messageInput = article.querySelector("[name='message']");
+        messageError = article.querySelector(".messageInput-result");
+        charcounter = article.querySelector(".charcounter");
+        maxchars = messageInput.getAttribute("maxlength");
+
+        output_dialog.showModal();
+    })
+    .catch((error) => {
+            handleError(error);
+    });
 }
 
-messageInput.addEventListener("input", (e) => {
+article.addEventListener("input", () => {
+    
+    const charcounter = article.querySelector(".charcounter");
+    const maxchars = messageInput.getAttribute("maxlength");
+
     let numOfEnteredChars = messageInput.value.length;
     charcounter.textContent = numOfEnteredChars + "/" + maxchars;
 
@@ -31,22 +53,27 @@ messageInput.addEventListener("input", (e) => {
     }
 });
 
-sendmessage.addEventListener("click", (event) => {
+footer.addEventListener("click", (e) => {
+    if (!e.target.matches(".send")) return;
+    
     if (messageInput.validity.valueMissing) {
         messageError.textContent = "Enter a message";
         messageError.style.color = "red";
         return;
     }
+    
+    const form = output_dialog.querySelector("form");
+
     const formData = new FormData(form);
     const formObj = Object.fromEntries(formData);
-    callAPI(endpoint, method, formObj)
+    callAPI(endpoint, 'POST', formObj)
         .then(() => {
+            const sendresult = footer.querySelector(".send-result");
             sendresult.textContent = "Successfully Sent";
             sendresult.style.color = "green";
-            sendmessage.disabled = true;
+            e.target.disabled = true;
         })
         .catch((error) => {
-            messageError.textContent = error;
-            messageError.style.color = "red";
+            handleError(error);
         });
 });
