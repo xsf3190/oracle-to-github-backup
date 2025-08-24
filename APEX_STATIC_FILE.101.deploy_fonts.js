5ac5d53c803b5ba86842c01c439ce8fd113fba41
mark brown <mark.russellbrown@gmail.com>
2025-08-24 23:00:23 +0200

    Updated using DBMS_CLOUD_REPO by user "EXAMPLE" from database HL7OFFZWEZQ2CAL_DB202103270929

--- APEX_STATIC_FILE.101.deploy_fonts.js
+++ APEX_STATIC_FILE.101.deploy_fonts.js
@@ -1,19 +1,48 @@
 import { dialog_header, dialog_article } from "deploy_elements";
 import { callAPI, handleError } from "deploy_callAPI";
 
-export const init = () => {
+export const init = (fontfaces) => {
+
+   for (const fontface of fontfaces) {
+        const fontFile = new FontFace(fontface.family,fontface.url);
+        document.fonts.add(fontFile);
+        fontFile.load();
+    };
+    
+
     dialog_header.addEventListener("click", async (e) => {
-        if (e.target.matches(".variable")) {
-            const svg = e.target.querySelector("svg");
-            svg.classList.toggle("visually-hidden");
-        }
+        if (!e.target.matches(".variable")) return;
+
+        const svg = e.target.querySelector("svg");
+        svg.classList.toggle("visually-hidden");
+
+        const variable = svg.classList.contains("visually-hidden") ? 0 : 1;
+        
+        const categories = dialog_article.querySelectorAll("[id$='category']");
+        categories.forEach((category) => {
+            const selected = category.options[category.selectedIndex].value;
+            const context = category.getAttribute("name").split("_")[0];
+            const query = "?category=" + selected + "&font=0&context=" + context + "&variable=" + variable;
+
+            callAPI('fonts/:ID','GET', query)
+                .then((data) => {
+                    const family = category.parentElement.querySelector("select:last-of-type");
+                    let index = family.options.length;
+                    while (index--) {
+                        family.remove(index);
+                    }
+                    family.insertAdjacentHTML('beforeend',data.content);
+                })
+                .catch((error) => {
+                    handleError(error);
+                });
+        });
     });
 
     dialog_article.addEventListener("change", (e) => {
         const name = e.target.getAttribute("name");
         const context = name.split("_")[0];
-        const loader = e.target.closest("fieldset").querySelector(".loader");
-        const icon = e.target.querySelector(".variable>.icon");
+        const icon = dialog_header.querySelector(".variable>.icon");
         const variable = icon.classList.contains("visually-hidden") ? 0 : 1;
 
         /* 
@@ -40,7 +69,7 @@ export const init = () => {
             if (!e.target.value) {
                 return;
             }
-
+             const loader = e.target.closest("fieldset").querySelector(".loader");
             loader.style.opacity=1;
             const query = "?category=&font=" + e.target.value + "&variable=" + variable;
             callAPI('fonts/:ID','GET', query)
